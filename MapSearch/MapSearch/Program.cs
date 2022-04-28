b90a60416da0639b13608834a4bd2e0a7df6c651
ken-kaneki <msherin@kse.org.ua>
2022-04-28 11:44:53 +0200

    Haversine func added, simple search algorithm done

--- MapSearch/MapSearch/Program.cs
+++ MapSearch/MapSearch/Program.cs
@@ -1,1 +1,91 @@
-﻿
+﻿class Program
+{
+    static double Haversine(float lat1, float lat2, float lon1, float lon2)
+    {
+        var R = 6371e3; // metres
+        var φ1 = lat1 * Math.PI/180; // φ, λ in radians
+        var φ2 = lat2 * Math.PI/180;
+        var Δφ = (lat2-lat1) * Math.PI/180;
+        var Δλ = (lon2-lon1) * Math.PI/180;
+
+        var a = Math.Sin(Δφ/2) * Math.Sin(Δφ/2) +
+            Math.Cos(φ1) * Math.Cos(φ2) *
+            Math.Sin(Δλ/2) * Math.Sin(Δλ/2);
+        var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1-a));
+
+        var d = (R * c) / 1000; // in kilometers
+        return d;
+    }
+
+    static void Main(string[] args)
+    {
+        var LATITUDE = 0;
+        var LONGITUDE = 1;
+        var CATEGORY = 2;
+        var TYPE = 3;
+        var NAME = 4;
+        var STREET = 5;
+        var BUILDING = 6;
+        var lat = new float();
+        var lon = new float();
+        var radius = new int();
+        var path = "";
+
+        foreach (var arg in args)
+        {
+            if (arg.Contains("--lat"))
+            {
+                lat = float.Parse(arg.Substring(6));
+            }
+            
+            else if (arg.Contains("--long"))
+            {
+                lon = float.Parse(arg.Substring(7));
+            }
+            
+            else if (arg.Contains("--size"))
+            {
+                radius = int.Parse(arg.Substring(7));
+            }
+            
+            else if (arg.Contains("--db"))
+            {
+                path = arg.Substring(5);
+            }
+        }
+        
+        List<string[]> data = new List<string[]>();
+        string[] lines = System.IO.File.ReadAllLines(path);
+        for (var i = 0; i < lines.Length; i++)
+        {
+            var line = lines[i];
+            string[] columns = line.Split(';');
+            data.Add(columns);
+        }
+
+        List<string[]> results = new List<string[]>();
+        for (var i = 0; i < data.Count; i++)
+        {
+            var goalLat = float.Parse(data[i][0]);
+            var goalLon = float.Parse(data[i][1]);
+            var distance = Haversine(lat, goalLat, lon, goalLon);
+            if (distance < radius)
+            {
+                results.Add(data[i]);
+            }
+        }
+
+        var j = 1;
+        foreach (var result in results)
+        {
+            Console.WriteLine(j + ". lat: " + result[LATITUDE] + ", lon: " + result[LONGITUDE] + ", category: " + result[CATEGORY]
+                              + ", type: " + result[TYPE] + ", name: " + result[NAME] + ", street: " + result[STREET] + ", building: " + result[BUILDING]);
+            j++;
+        }
+
+        Console.ReadLine();
+    }
+}
+
+//MapSearch.exe --db=ukraine_poi.csv --lat=30,212 --long=35,872 --size=2000
+
